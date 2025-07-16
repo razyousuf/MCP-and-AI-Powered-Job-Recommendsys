871158b363a303e51ffb9a1017459d58a6e3d8c4
razyousuf <23010492@hope.ac.uk>
2025-07-16 01:04:55 +0100

    Apify token, LinkedIn and Indeed scraping functions added

--- job_recommender/helper_func.py
+++ job_recommender/helper_func.py
@@ -3,7 +3,17 @@ import sys
 from exception.exception_handler import AppException 
 from dotenv import load_dotenv
 import fitz  # PyMuPDF
+from openai import OpenAI
+from apify_client import ApifyClient
 
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+# Initialize the ApifyClient with your API token
+client = ApifyClient("APIFY_API_TOKEN")
 def extract_text_from_pdf(pdf_file: str) -> str:
     """
     Extract text from a PDF file.
@@ -22,3 +32,68 @@ def extract_text_from_pdf(pdf_file: str) -> str:
         return text
     except AppException as e:
         raise AppException(e, sys) from e
+
+
+def prompt_to_openai(prompt, max_tokens=600):
+    """
+    Send a prompt to OpenAI's API and get the response.
+    
+    Args:
+        prompt (str): The prompt to send.
+        max_tokens (int): Maximum number of tokens in the response.
+        
+    Returns:
+        str: Response from OpenAI's API.
+    """
+    try:
+        response = client.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                {"role": "system", "content": "You are a helpful job-related assistant."},
+                {"role": "user", "content": prompt}
+            ],
+            max_tokens=max_tokens,
+            temperature=0.5
+        )
+
+        return response.choices[0].message['content'] #.content
+    except AppException as e:
+        raise AppException(e, sys) from e
+
+
+# Get job listings from LinkedIn using a search query and location.
+def get_job_from_linkedin(search_query, location = "United Kingdom", num_results = 50):
+    run_input = {
+        "title": search_query,
+        "location": location,
+        "rows": num_results,
+        "proxy": {
+            "useApifyProxy": True,
+            "apifyProxyGroups": ["RESIDENTIAL"]
+        }
+    }
+    run = client.actor("BHzefUZlZRKWxkTck").call(run_input=run_input)
+    job = list(client.dataset(run["defaultDatasetId"]).iterate_items())
+    if not job:
+        raise AppException("No job listings found for the given search query and location.")
+    return job
+
+# Get job listings from Indeed using a search query and location.
+def get_job_from_indeed(search_query, location = "United Kingdom", num_results = 50):
+    run_input = {
+        "query": search_query,
+        "country": location,
+        "maxRows": num_results,
+        "sort": "relevance",
+        "proxy": {
+            "useApifyProxy": True,
+            "apifyProxyGroups": ["RESIDENTIAL"]
+        }
+    }
+
+    run = client.actor("MXLpngmVpE8WTESQr").call(run_input=run_input)
+
+    job = list(client.dataset(run["defaultDatasetId"]).iterate_items())
+    if not job:
+        raise AppException("No job listings found for the given search query and location.")
+    return job
