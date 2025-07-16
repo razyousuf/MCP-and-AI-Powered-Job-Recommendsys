53610ee0e95e5562689555c0e40ef01d3424d9a4
razyousuf <23010492@hope.ac.uk>
2025-07-16 20:21:15 +0100

    Job APIs code included

--- job_recommender/helper_func.py
+++ job_recommender/helper_func.py
@@ -4,7 +4,6 @@ from exception.exception_handler import AppException
 from dotenv import load_dotenv
 import fitz  # PyMuPDF
 from openai import OpenAI
-from apify_client import ApifyClient
 
 load_dotenv()
 
@@ -12,8 +11,6 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 client = OpenAI(api_key=OPENAI_API_KEY)
 
-# Initialize the ApifyClient with your API token
-client = ApifyClient("APIFY_API_TOKEN")
 def extract_text_from_pdf(pdf_file: str) -> str:
     """
     Extract text from a PDF file.
@@ -61,39 +58,3 @@ def prompt_to_openai(prompt, max_tokens=600):
         raise AppException(e, sys) from e
 
 
-# Get job listings from LinkedIn using a search query and location.
-def get_job_from_linkedin(search_query, location = "United Kingdom", num_results = 50):
-    run_input = {
-        "title": search_query,
-        "location": location,
-        "rows": num_results,
-        "proxy": {
-            "useApifyProxy": True,
-            "apifyProxyGroups": ["RESIDENTIAL"]
-        }
-    }
-    run = client.actor("BHzefUZlZRKWxkTck").call(run_input=run_input)
-    job = list(client.dataset(run["defaultDatasetId"]).iterate_items())
-    if not job:
-        raise AppException("No job listings found for the given search query and location.")
-    return job
-
-# Get job listings from Indeed using a search query and location.
-def get_job_from_indeed(search_query, location = "United Kingdom", num_results = 50):
-    run_input = {
-        "query": search_query,
-        "country": location,
-        "maxRows": num_results,
-        "sort": "relevance",
-        "proxy": {
-            "useApifyProxy": True,
-            "apifyProxyGroups": ["RESIDENTIAL"]
-        }
-    }
-
-    run = client.actor("MXLpngmVpE8WTESQr").call(run_input=run_input)
-
-    job = list(client.dataset(run["defaultDatasetId"]).iterate_items())
-    if not job:
-        raise AppException("No job listings found for the given search query and location.")
-    return job
